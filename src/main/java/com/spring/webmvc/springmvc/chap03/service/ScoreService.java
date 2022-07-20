e9027485cb3d0341e56243453f6e0d486415eb4d
jyeonki <kay8825@naver.com>
2022-07-20 11:41:45 +0900

    [220720] # First interim commit

--- src/main/java/com/spring/webmvc/springmvc/chap03/service/ScoreService.java
+++ src/main/java/com/spring/webmvc/springmvc/chap03/service/ScoreService.java
@@ -10,6 +10,7 @@ import java.util.List;
 // Controller & Repository 중간 로직 처리
 @Service // 스프링 빈 등록
 @RequiredArgsConstructor
+// final or @NotNull 이 붙은 필드의 생성자를 자동 생성해주는 lombok 어노테이션
 public class ScoreService {
 
     private final ScoreRepository repository;
@@ -45,10 +46,16 @@ public class ScoreService {
         for (Score s : scoreList) {
             String original = s.getName();
             StringBuilder markName = new StringBuilder(String.valueOf(original.charAt(0)));
-            for (int i = 0; i < original.length() - 1; i++) {
-                markName.append("*");
+            // ex) 이름이 김지연이면 charAt(0) -> 김 (char type)
+            // char 타입을 다시 String으로 형변환 -> String.valueOf
+
+            // String끼리의 덧셈(+)은 메모리 할당과 메모리 해제를 발생시킨다, 성능상 좋지 않다. 그래서 사용하는게 StringBuilder!
+            // StringBuilder - 문자열 덧셈시 사용, append를 사용해서 문자열을 더한다. 출력 시에는 toString()을 붙여야 한다.
+
+            for (int i = 0; i < original.length() - 1; i++) { // 0, 1
+                markName.append("*"); // ex) 이름이 세글자면 두글자에 *이 붙어야한다 (반복문 2번 돌려주기) -> 김**
             }
-            s.setName(markName.toString());
+            s.setName(markName.toString()); // StringBuilder - 출력 시에는 toString()을 붙여야 한다
         }
     }
 }
